b980161ac98de444b33e7dfe1a04da8a50728966
Mhai Laine Bringuelo <115721615+MhaiLaine@users.noreply.github.com>
2024-11-21 01:21:54 +0800

    mhais part

--- FinalProject/Scripts/AngularScripts/Controller.js
+++ FinalProject/Scripts/AngularScripts/Controller.js
@@ -1,4 +1,22 @@
-﻿app.controller("FinalProjectController", function ($scope, FinalProjectService) {
+﻿app.controller("FinalProjectController", function ($scope, $window) {
+    // 1. Main menu 
+    $scope.redirectToRegistration = function () {
+        $window.location.href = "/Home/RegistrationPage";
+    };
+
+    $scope.redirectToLogIn = function () {
+        $window.location.href = "/Home/LogInPage";
+    };
+
+    // 2. Registration Page
+
+
+
+
+
+
+
+
 
 
 
